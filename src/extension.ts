47e837f02e0d9ad3c87294ff7af67e3924a57218
Arnovda1 <arno@van-der-auwera.com>
2026-05-12 00:02:57 +0200

    update

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,22 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sqlite-editor" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('sqlite-editor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from sqlite-editor!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.window.registerCustomEditorProvider(
+			'sqlite-editor.dbEditor',
+			new YourEditorProvider(),
+		)
+	);
 }
 
-// This method is called when your extension is deactivated
+class YourEditorProvider implements vscode.CustomReadonlyEditorProvider {
+	openCustomDocument(uri: vscode.Uri) { return { uri, dispose() {} }; }
+	resolveCustomEditor(document: vscode.CustomDocument, webviewPanel: vscode.WebviewPanel) {
+		webviewPanel.webview.options = { enableScripts: true };
+		// read file bytes: fs.readFileSync(document.uri.fsPath)
+		// pass to sql.js, query tables, render HTML
+		webviewPanel.webview.html = '...your HTML...';
+	}
+}
+
 export function deactivate() {}
