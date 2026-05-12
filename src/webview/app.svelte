40447ad2745fe96c75302f6444979d678d9e59bb
Arnovda1 <arno@van-der-auwera.com>
2026-05-12 04:16:36 +0200

    update

--- src/webview/app.svelte
+++ src/webview/app.svelte
@@ -50,8 +50,8 @@
     loading = false;
   })();
 
-  async function getTables(): Promise<string[]> {
-    const result = await query(`SELECT name FROM sqlite_master WHERE type IN ('table', 'view') ORDER BY name`);
+  async function getTables(type: `'table'` | `'view'` | `'table', 'view'` = `'table', 'view'`): Promise<string[]> {
+    const result = await query(`SELECT name FROM sqlite_master WHERE type IN (${type}) ORDER BY name`);
     if (!result || 'error' in result) return [];
     return result.rows.map((r) => r[0] as string);
   }
