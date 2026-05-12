14799e8342c80f9e075dfd8b51c888f36e53af34
Arnovda1 <arno@van-der-auwera.com>
2026-05-12 03:46:46 +0200

    update

--- src/webview/components/result-table.svelte
+++ src/webview/components/result-table.svelte
@@ -42,6 +42,12 @@
               </td>
             {/each}
           </tr>
+        {:else}
+          <tr>
+            <td colspan={data.columns.length} class="px-3 py-4 text-center text-gray-500 dark:text-gray-400 italic">
+              No results found
+            </td>
+          </tr>
         {/each}
       </tbody>
     </table>
